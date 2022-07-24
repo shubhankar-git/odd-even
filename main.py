6eecf1b8c92827f6c20e7250644e93223c290b84
shubhankar-git <108572107+shubhankar-git@users.noreply.github.com>
2022-07-24 17:08:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ st.set_page_config(page_title=string, page_icon="🔍")
 
 st.title('Odd-Even Finder')
 x = st.number_input('Enter a number')
-if x%2==0:st.write("your number is even")
-else:("your number is odd")
+if (x).is_integer():
+    if x%2==0:st.write("your number is even")
+    elif x%2!=0:st.write("your number is odd")
+else:st.write("please enter integer number")
